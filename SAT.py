93fa4215ae9eb70666a300846a29530c93e95949
Leonardo <leobol96@gmail.com>
2020-09-03 22:31:44 +0200

    Optimization and control of the box around the cell

--- SAT.py
+++ SAT.py
@@ -11,26 +11,38 @@ def readRules():
 def encodePuzle(dimension):
     # Creating an array N * N dimension with only zeros
     sudoku = np.zeros((dimension, dimension)) 
-    with open("sudoku-example_6_6.txt","r") as puzle_file:
+    with open("sudoku-example.txt","r") as puzle_file:
         for line in puzle_file:
             # Filling the matrix with the numbers token from the file 
             sudoku[int(line[0])-1,int(line[1])-1] = int(line[2])
         return sudoku
 
+# Check if the value is in the row
+def check_row(arr, row, num, dimension): 
+    for i in range(dimension): 
+        if(arr[row][i] == num): 
+            return True
+    return False
+  
+# Check if the value is in the col
+def check_col(arr, col, num, dimension): 
+    for i in range(dimension): 
+        if(arr[i][col] == num): 
+            return True
+    return False
+  
+# Check if the value is in the box 
+def check_box(arr, row, col, num): 
+    for i in range(3): 
+        for j in range(3): 
+            if(arr[i + row][j + col] == num): 
+                return True
+    return False
 
-# Returns true if the sudoku passed as a parameter follows the rules of the game  
-def checkValidity(sudoku,dimension):
-    # For every sudoku's cell 
-    for idx_col in range(dimension):
-        for idx_row in range(dimension):
-            for k in range(dimension):
-                #col
-                if (idx_col != k and sudoku[idx_col][idx_row] == sudoku[k][idx_row]):
-                    return False
-                #row
-                if (idx_row != k and sudoku[idx_col][idx_row] == sudoku[idx_col][k]):
-                    return False        
-    return True
+# Row + col + box
+def check_all(arr, row, col, num, dimension): 
+    return not check_row(arr, row, num, dimension) and not check_col(arr, col, num, dimension) and not check_box(arr, row - row % 3, col - col % 3, num) 
+  
             
 # This function used the Davis Putman algorithm to solve the sat problem
 def DP_algorithm():
@@ -41,7 +53,7 @@ def heuristic_algorithm_recursive(sudoku,dimension,col,row):
 
     # Last element of the sudoku 
     if (col == dimension - 1 and row == dimension):
-        return checkValidity(sudoku,dimension)
+        return True
           
     # Last element of the row
     if (row == dimension):
@@ -54,11 +66,12 @@ def heuristic_algorithm_recursive(sudoku,dimension,col,row):
     
     # Recursion
     for k in range(1, dimension + 1):
-        sudoku[col][row] = k
-        if (heuristic_algorithm_recursive(sudoku,dimension,col,row + 1)):
-            return True
+        # Check if the sudoku is valid with the new value
+        if (check_all(sudoku,col,row,k,dimension)):
+            sudoku[col][row] = k
+            if (heuristic_algorithm_recursive(sudoku,dimension,col,row + 1)):
+                return True
         sudoku[col][row] = 0
-
     return False
 
 # This will be the second heuristic method to solve the SAT problem
@@ -68,7 +81,7 @@ def heuristic_algorithm_two():
 # Main function  
 if __name__=="__main__": 
     
-    dimension = 6
+    dimension = 9
     sudoku = encodePuzle(dimension)
     print("Sudoku to solve")
     print(sudoku)
